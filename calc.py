f46365e3ee12ca30df3760646d42db1c43666303
Ania Bieniek <anek12335@wp.pl>
2023-10-21 09:00:00 +0200

    calc-test.py

--- calc.py
+++ calc.py
@@ -1,53 +1,40 @@
+
 import sys
 import logging
 logging.basicConfig(level=logging.DEBUG)
 
       
 def add(x, y):
-    return (x + y)
+    return(x + y)
 
 def sub(x, y):
-    return (x - y)
+    return(x - y)
 
 def multiply(x, y):
-    return (x * y)
+    return(x * y)
            
 def divide(x, y):
-    return (x / y) 
+    return(x / y) 
 
-operations = {
-    "1":add,
-    "2":sub,
-    "3":multiply,
-    "4":divide
-}
+calc = add, sub, multiply, divide
 
 def start():
     while True:
         choice = int(input("Podaj działanie posługując się odpowiednią liczbą: 1 Dodawanie 2 Odejmowanie 3 Mnożenie 4 Dzielenie:"))
-        if int(choice) > 4:
-          exit(1)
         x = float(input("Wpisz 1 cyfrę:"))
-        y = float(input("Wpisz 2 cyfrę:"))
-        result = operations[(choice)](x,y)
+        y = float(input("Wpisz 2 cyfrę"))
+        choice = int(choice)
+        if choice == 1:
+          calc[0](x, y) 
+        elif choice == 2:
+            calc[1](x, y)
+        elif choice == 3:
+            calc[2](x, y)
+        elif choice == 4:
+            calc[3](x, y)
+        result = calc(x,y)
         print(result)
-        
-
+       
 if __name__ == "__main__":
     start()
-    operations()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    calc()
--- calculating.py
+++ calculating.py
@@ -6,16 +6,12 @@ logging.basicConfig(level=logging.DEBUG)
 
 def calc(x, y, choice):
     if choice == 1:
-        logging.info(f"Dodaję {x} i {y}")
         print(x + y)
     elif choice == 2:
-        logging.info(f"Odejmuję {y} od {x}")
         print(x - y)
     elif choice == 3:
-        logging.info(f"Mnożę {x} i {y}")
         print(x * y)
     elif choice == 4:
-        logging.info(f"Dzielę {x} przez {y}")
         print(x / y) 
    
 
